cc9d355bfd80213a7a3d94c2a5232b8cfbfeacf2
FrBarbarossa <91803857+FrBarbarossa@users.noreply.github.com>
2021-11-18 22:43:33 +0300

    Project started
    Created work with database, but passwords are bad.

---
+++ Dataworkspace.py
@@ -0,0 +1,55 @@
+import sqlite3
+import bcrypt
+
+salt = bcrypt.gensalt()
+
+conn = sqlite3.connect('usersdat.db')
+cur = conn.cursor()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS users(
+   userid INTEGER PRIMARY KEY,
+   username TEXT,
+   password TEXT);
+""")
+conn.commit()
+cur.execute("""CREATE TABLE IF NOT EXISTS folders(
+   userid INTEGER PRIMARY KEY,
+   folderplace TEXT);
+""")
+conn.commit()
+
+
+def insert_user(name: str, password: str) -> bool:
+    user = list(cur.execute("SELECT userid FROM users WHERE username=(?)", (name,)))
+    if not user:
+        newfolder = ("Asds",)  # Формирование ссылки на таблицу
+        psw = bcrypt.hashpw(password.encode(), salt)
+        user = (name, psw)
+        cur.execute("INSERT INTO users (username, password) VALUES(?,?);", user)
+        cur.execute("INSERT INTO folders (folderplace) VALUES(?);", newfolder)
+        conn.commit()
+        return True
+    return False
+
+
+def get_folder(name: str) -> str:
+    user_id = list(cur.execute("SELECT userid FROM users WHERE username=(?)", (name,)))
+    if user_id:
+        user_id = user_id[0][0]
+        folderlink = list(cur.execute("SELECT folderplace FROM folders WHERE userid=(?)", (user_id,)))[0][0]
+        print(folderlink)
+
+
+def login(name: str, password: str) -> bool:
+    user = list(cur.execute("SELECT password FROM users WHERE username=(?)", (name,)))
+    if user:
+        psw = bcrypt.hashpw(password.encode(), salt)
+        print(user[0][0], psw)
+        if user[0][0] == psw:
+            return True
+    return False
+
+
+print(insert_user('ali2', '123'))
+print(login('ali2', '123'))
+# print(insert_user('BLANK', '1234fas'))
